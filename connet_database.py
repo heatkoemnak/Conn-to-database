b08982fadd104addc1fcea87dec858bdb9ef6a13
koemnak <heatkimnak@gmail.com>
2022-09-11 19:49:52 +0700

    commit

--- connet_database.py
+++ connet_database.py
@@ -1,12 +1,28 @@
 import mysql.connector as connector
+
 try:
-    conn=connector.connect(user='root',localhost='172.0.0.1',password='',batabase='hdsd')
-    # cursor=connect.cursor()
-    sql="INSERT INTO `db`(`studentname`, `email`, `place`) VALUES ('koemank','heatkoemnak@gmail.com','pursat')"
-    cursor = conn.cursor()
-    conn.cursor(sql)
-    conn.commit()
+    connection = connector.connect(host='localhost',
+                                         database='hdsd',
+                                         user='root',
+                                         password='')
+
+    mySql_insert_query = """INSERT INTO db (id,studentname,email, place) 
+                           VALUES (%s,%s, %s, %s) """
+
+    records_to_insert = [('1','HP Pavilion Power', 1999, '2019-01-11'),
+                         ('2','MSI WS75 9TL-496', 5799, '2019-02-27'),
+                         ('3','Microsoft Surface', 2330, '2019-07-23')]
+
+    cursor = connection.cursor()
+    cursor.executemany(mySql_insert_query, records_to_insert)
+    connection.commit()
     print(cursor.rowcount, "Record inserted successfully into Laptop table")
-    cursor.close()
-except connector.error as err:
-    print(err)
+
+except mysql.connector.Error as error:
+    print("Failed to insert record into MySQL table {}".format(error))
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")

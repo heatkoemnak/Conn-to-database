3daf85ea6b9cdc2ce6beb6ea6d4cad1a0b667cbc
koemnak <heatkimnak@gmail.com>
2022-09-11 19:37:50 +0700

    commit

--- connet_database.py
+++ connet_database.py
@@ -2,7 +2,7 @@ import mysql.connector as connector
 try:
     conn=connector.connect(user='root',localhost='172.0.0.1',password='',batabase='hdsd')
     # cursor=connect.cursor()
-    sql=INSERT INTO `db`(`studentname`, `email`, `place`) VALUES ('koemank','heatkoemnak@gmail.com','pursat')
+    sql="INSERT INTO `db`(`studentname`, `email`, `place`) VALUES ('koemank','heatkoemnak@gmail.com','pursat')"
     cursor = conn.cursor()
     conn.cursor(sql)
     conn.commit()

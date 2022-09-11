3e91b7ba16346405c30368ad376103e156ad4679
koemnak <heatkimnak@gmail.com>
2022-09-11 19:36:26 +0700

    commit

---
+++ connet_database.py
@@ -0,0 +1,12 @@
+import mysql.connector as connector
+try:
+    conn=connector.connect(user='root',localhost='172.0.0.1',password='',batabase='hdsd')
+    # cursor=connect.cursor()
+    sql=INSERT INTO `db`(`studentname`, `email`, `place`) VALUES ('koemank','heatkoemnak@gmail.com','pursat')
+    cursor = conn.cursor()
+    connection.cursor(sql)
+    conn.commit()
+    print(cursor.rowcount, "Record inserted successfully into Laptop table")
+    cursor.close()
+except connector.error as err:
+    print(err)
